6b9b2634f9bc0a009f81a39d89f1bda2cbb96673
Timothy Renner <renner.timothy@gmail.com>
2014-11-29 17:25:30 -0600

    Implementation completed.

--- src/DynamicalSystems.jl
+++ src/DynamicalSystems.jl
@@ -1,5 +1,52 @@
 module DynamicalSystems
 
-# package code goes here
+# Creates the functions for the harmonic oscillator with a sinusoidal driving
+# force.
+#
+# x'' + beta x' + omega^2 x = gamma sin(omega t)
+#
+# Args:
+#   omega0: The frequency term.
+#   beta: The damping term (default: 0.0).
+#   gamma: The driving force amplitude (default: 0.0).
+#   omega: The driving force frequency (default: 1.0).
+#
+# Returns: An array of two functions defining the harmonic oscillator system.
+function harmonicOscillator(omega0::FloatingPoint;
+    beta::FloatingPoint=0.0, gamma::FloatingPoint=0.0,
+    omega::FloatingPoint=1.0)
+
+    return [(t,x) -> x[2],
+            (t,x) -> (gamma*sin(omega*t) -
+                 beta*x[2] -
+                 (omega0^2)*x[1])];
+
+end #Close simpleHarmonicOscillator.
+
+# Creates the functions for a Duffing oscillator.
+#
+# x'' + delta x' + alpha x + beta x^3 = gamma sin(omega t)
+# 
+# Args:
+#   delta: The damping term.
+#   gamma: The driving amplitude.
+#   alpha: The stiffness (default: 1.0).
+#   beta: The restoring force non-linearity (default: -1.0).
+#   omega: The driving force frequency (default: 1.0).
+#
+# Returns: An array of two functions defining the Duffing oscillator system.
+function duffingOscillator(delta::FloatingPoint, gamma::FloatingPoint;
+    alpha::FloatingPoint=1.0, beta::FloatingPoint=-1.0,
+    omega::FloatingPoint=1)
+
+    return [(t,x) -> x[2],
+     (t,x) -> (gamma*sin(omega*t) -
+              delta*x[2] -
+              alpha*x[1] -
+              beta*(x[1]^3))];
+
+end #Close duffingOscillator.
+
+export harmonicOscillator, duffingOscillator
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,40 @@
 using DynamicalSystems
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Test 1: harmonicOscillator.
+
+omega0_test1 = 2.0;
+beta_test1 = 0.5;
+gamma_test1 = 1.5;
+omega_test1 = 1.0;
+
+t_test1 = 3.0;
+x_test1 = [1.0, 2.0];
+
+functions_test1 = harmonicOscillator(omega0_test1, beta=beta_test1, 
+    gamma=gamma_test1, omega=omega_test1);
+
+# Validate the results of the functions.
+truth_test1 = [2.0, -4.788319987910199];
+answer_test1 = map(fe -> fe(t_test1, x_test1), functions_test1);
+
+@test_approx_eq answer_test1 truth_test1
+
+# Test 2: duffingOscillator.
+
+delta_test2 = 0.5;
+gamma_test2 = 1.5;
+alpha_test2 = 0.25;
+beta_test2 = -0.75;
+omega_test2 = 2.5;
+t_test2 = 3.0;
+x_test2 = [1.5, 3.5];
+
+functions_test2 = duffingOscillator(delta_test2, gamma_test2,
+    alpha=alpha_test2, beta=beta_test2, omega=omega_test2);
+
+# Validate the results of the functions.
+truth_test2 = [3.5, 1.8132499651621083];
+answer_test2 = map(fe -> fe(t_test2, x_test2), functions_test2);
+
+@test_approx_eq answer_test2 truth_test2
